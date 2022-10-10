76bd507dbde33ff95d9e189e0e798d985aa150ac
Majd Awad <majd.awad97@gmail.com>
2022-10-10 11:21:30 +0300

    Finished Project

--- lib/song_page.dart
+++ lib/song_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'neu_box.dart';
 
 class SongPage extends StatefulWidget {
@@ -18,10 +19,11 @@ class _SongPageState extends State<SongPage> {
             padding: const EdgeInsets.symmetric(horizontal: 25),
             child: Column(
               children: [
+                SizedBox(height: 10),
                 // back button and menu button
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
+                  children: const [
                     SizedBox(
                       height: 60,
                       width: 60,
@@ -39,13 +41,115 @@ class _SongPageState extends State<SongPage> {
                 ),
 
                 const SizedBox(height: 25),
+
                 // cover art , artist name , song name
+                NeuBox(
+                  child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Image.asset('images/cover_art.jpg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Michael Jackson',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                      color: Colors.grey.shade700),
+                                ),
+                                const SizedBox(
+                                  height: 6,
+                                ),
+                                const Text(
+                                  'Love Never Felt so Good',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const Icon(
+                              Icons.favorite,
+                              color: Colors.red,
+                              size: 32,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 25),
 
                 // start time , shuffle button , repeat button , end time
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text('0:00'),
+                    Icon(Icons.shuffle),
+                    Icon(Icons.repeat),
+                    Text('4:06'),
+                  ],
+                ),
+
+                const SizedBox(height: 25),
 
                 // linear bar
+                NeuBox(
+                  child: LinearPercentIndicator(
+                    lineHeight: 10,
+                    percent: 0.8,
+                    progressColor: Colors.green,
+                    backgroundColor: Colors.transparent,
+                  ),
+                ),
+
+                const SizedBox(height: 25),
 
                 // previous song , pause play , skip next song
+
+                SizedBox(
+                  height: 80,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: NeuBox(
+                            child: Icon(
+                          Icons.skip_previous,
+                          size: 32,
+                        )),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          child: NeuBox(
+                              child: Icon(
+                            Icons.play_arrow,
+                            size: 32,
+                          )),
+                        ),
+                      ),
+                      Expanded(
+                        child: NeuBox(
+                            child: Icon(
+                          Icons.skip_next,
+                          size: 32,
+                        )),
+                      ),
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
