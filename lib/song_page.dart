a4b05b2a98240bfc6103243d21d7624fef3e0c92
Majd Awad <majd.awad97@gmail.com>
2022-10-10 10:28:26 +0300

    created a box widget and making home screen

--- lib/song_page.dart
+++ lib/song_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'neu_box.dart';
 
 class SongPage extends StatefulWidget {
   const SongPage({super.key});
@@ -11,22 +12,43 @@ class _SongPageState extends State<SongPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            boxShadow: [
-              // darker shadow on the bottom right
-              BoxShadow(
-                color: Colors.grey.shade500,
-                blurRadius: 15,
-              ),
-            ],
+        backgroundColor: Colors.grey[300],
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              children: [
+                // back button and menu button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: NeuBox(
+                        child: Icon(Icons.arrow_back),
+                      ),
+                    ),
+                    Text('P L A Y L I S T'),
+                    SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: NeuBox(child: Icon(Icons.menu)),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 25),
+                // cover art , artist name , song name
+
+                // start time , shuffle button , repeat button , end time
+
+                // linear bar
+
+                // previous song , pause play , skip next song
+              ],
+            ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
